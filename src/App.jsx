6d9520552e5f4e5dba3ab0894df54cbd487305e7
Ritikpatidar78 <ritikpatidar7898634330@gmail.com>
2023-08-21 19:27:07 +0530

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Form from './components/Form'
 import Listgroup from './components/Listgroup'
 import { DataProvider } from './DataContext/DataContext'
